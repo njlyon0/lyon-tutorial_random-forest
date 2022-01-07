e1dbaa5c57fca0ad3aafb5a59950d1547681b97b
Nick J Lyon <nicholasjlyon@gmail.com>
2022-01-07 15:55:05 -0600

    Created functioning random forest code, added conditional permutation component, and script ends with exploratory plotting of variables identified by random forests

--- random forest tutorial.R
+++ random forest tutorial.R
@@ -37,7 +37,7 @@ data(varespec)
 data(varechem)
 
 # Get just one of the lichen species to use as a response variable
-varespec.v2 <- dplyr::select(varespec, Empenigr)
+varespec.v2 <- dplyr::select(varespec, Callvulg)
 
 # Bind the response and predictors together
 data.obj <- cbind(varechem, varespec.v2)
@@ -51,18 +51,98 @@ head(data.obj)
 ## --------------------------------------------------- ##
      # Part 2: Random Forest with randomForest ####
 ## --------------------------------------------------- ##
+# Run the random forest
+rf1 <- randomForest(Callvulg ~ .,
+                    # The 'Y ~ .' format uses all other columns as predictors
+                    # Makes formatting your data **crucial**
+                    data = data.obj,
+                    ntree = 1000,
+                    # How many trees should be in the forest
+                    mtry = 2,
+                    # mtry is # variables / node in tree
+                    na.action = na.omit,
+                    keep.forest = T,
+                    keep.inbag = T)
 
-
-
-
+# Create a variable importance plot
+randomForest::varImpPlot(x = rf1,
+                         sort = T,
+                         n.var = (ncol(data.obj) - 1),
+                         main = "Variable Importance")
+## Great!
 
 ## --------------------------------------------------- ##
  # Part 3: Conditional Permutation Importance (CPI) ####
 ## --------------------------------------------------- ##
+# See vignette for details:
+## cran.r-project.org/web/packages/permimp/vignettes/permimp-package.html
+
+# Let's implement conditional permutation
+  ## Should see a progress bar in the Console after you run the above
+rf1.hiCond <- permimp::permimp(object = rf1,
+                               conditional = T,
+                               threshold = 0.95,
+                               do_check = F)
+
+# Plot distribution of importance
+  ## Note this step may take a minute or two
+plot(rf1.hiCond, type = "box", horizontal = T)
+
+# Re-fit with a lower threshold
+rf1.loCond <- permimp::permimp(object = rf1,
+                               conditional = T,
+                               threshold = 0.55,
+                               do_check = F)
+
+# Plot distribution of this importance
+plot(rf1.loCond, type = "box", horizontal = T)
+
+# Make objects of the importance values
+rf1.loCond.vals <- data.frame(rf1.loCond$values)
+rf1.hiCond.vals <- data.frame(rf1.hiCond$values)
+
+# Side by side comparison of ranking
+  ## Ignore warning message
+rownames(rf1.loCond.vals)[order(-rf1.loCond.vals)]
+rownames(rf1.hiCond.vals)[order(-rf1.hiCond.vals)]
+
+## --------------------------------------------------- ##
+          # Part 4: Exploratory Plotting ####
+## --------------------------------------------------- ##
+# This part is just for fun
+
+# Set custom aesthetic
+pref_theme <- theme_classic() + theme(axis.text = element_text(size = 13),
+                                      axis.title = element_text(size = 15),
+                                      legend.position = "none")
+
+# Graph the top few metrics
+ggplot(data.obj, aes(y = Callvulg, x = pH)) +
+  geom_point(color = '#a6cee3') +
+  geom_smooth(method = 'lm', color = 'black') +
+  labs(x = "pH", y = "Callvulg Cover (%)") +
+  pref_theme
 
 
+ggplot(data.obj, aes(y = Callvulg, x = Al)) +
+  geom_point(color = '#b2df8a') +
+  geom_smooth(method = 'lm', color = 'black') +
+  labs(x = "Aluminum", y = "Callvulg Cover (%)") +
+  pref_theme
 
 
+ggplot(data.obj, aes(y = Callvulg, x = P)) +
+  geom_point(color = '#b2df8a') +
+  geom_smooth(method = 'lm', color = 'black') +
+  labs(x = "Phosphorous", y = "Callvulg Cover (%)") +
+  pref_theme
+
+
+ggplot(data.obj, aes(y = Callvulg, x = Baresoil)) +
+  geom_point(color = '#33a02c') +
+  geom_smooth(method = 'lm', color = 'black') +
+  labs(x = "Baresoil (%)", y = "Callvulg Cover (%)") +
+  pref_theme
 
 # END ####
 
